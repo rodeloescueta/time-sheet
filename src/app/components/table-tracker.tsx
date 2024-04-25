5e62d14fdddefc48cf4be532057d8100b43aaf86
Rodelo Escueta <rodelo.escueta@outgive.ca>
2024-04-25 12:01:18 +0800

    add loading

--- src/app/components/table-tracker.tsx
+++ src/app/components/table-tracker.tsx
@@ -4,38 +4,48 @@ import { useProjectStore } from "../store";
 import LogTime from "./log-time";
 
 function TableTracker() {
-  const projects = useProjectStore((state) => state.projects);
+  const { projects, isLoading } = useProjectStore((state) => ({
+    projects: state.projects,
+    isLoading: state.isLoading,
+  }));
   return (
     <div className="p-8 ">
       <pre>{JSON.stringify(projects, null, 2)}</pre>
-      <table className="border min-w-full divide-y divide-gray-200">
-        <thead className="bg-gray-50">
-          <tr>
-            <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
-              Project Name
-            </th>
-            <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
-              Hours Worked
-            </th>
-          </tr>
-        </thead>
-        <tbody className="bg-white divide-y divide-gray-200">
-          {projects.map((el) => {
-            return (
-              <tr key={el.project}>
-                <td className="px-6 py-4">
-                  <div className="text-sm text-gray-900">{el.project}</div>
-                </td>
-                <td className="px-6 py-4 whitespace-nowrap">
-                  <span className="px-2 inline-flex text-xs leading-5 font-semibold rounded-full bg-green-100 text-green-800">
-                    {el.hours}
-                  </span>
-                </td>
-              </tr>
-            );
-          })}
-        </tbody>
-      </table>
+      {isLoading ? (
+        <button type="button" className="bg-blue-500" disabled>
+          Processing...
+        </button>
+      ) : (
+        <table className="border min-w-full divide-y divide-gray-200">
+          <thead className="bg-gray-50">
+            <tr>
+              <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                Project Name
+              </th>
+              <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                Hours Worked
+              </th>
+            </tr>
+          </thead>
+          <tbody className="bg-white divide-y divide-gray-200">
+            {projects.map((el) => {
+              return (
+                <tr key={el.project}>
+                  <td className="px-6 py-4">
+                    <div className="text-sm text-gray-900">{el.project}</div>
+                  </td>
+                  <td className="px-6 py-4 whitespace-nowrap">
+                    <span className="px-2 inline-flex text-xs leading-5 font-semibold rounded-full bg-green-100 text-green-800">
+                      {el.hours}
+                    </span>
+                  </td>
+                </tr>
+              );
+            })}
+          </tbody>
+        </table>
+      )}
+
       <LogTime />
     </div>
   );
--- src/app/store.ts
+++ src/app/store.ts
@@ -3,6 +3,7 @@ import { persist } from "zustand/middleware";
 
 type ProjectStore = {
   projects: { project: string; hours: string }[];
+  isLoading: boolean;
   addProject: (project: string, hours?: string) => Promise<void>;
   removeProject: (project: string, hours?: string) => Promise<void>;
 };
@@ -11,9 +12,12 @@ export const useProjectStore = create<ProjectStore>()(
   persist(
     (set) => ({
       projects: [],
+      isLoading: false,
       addProject: async (project, hours) => {
+        set(() => ({ isLoading: true }));
         await new Promise((resolve) => setTimeout(resolve, 1000)); // simulate delay
         let proj = { project: project, hours: hours ? hours : "0" };
+        set(() => ({ isLoading: false }));
         set((s) => ({ projects: [...s.projects, proj] }));
       },
       removeProject: async (project) => {
